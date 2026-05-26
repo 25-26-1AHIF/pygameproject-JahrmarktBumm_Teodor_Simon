3166233d7a80abbe54d9b737e34ae5e4287eac80
SimonAmann <simon.amann@student.htl-rankweil.at>
2026-05-26 10:34:40 +0200

    Target versuch

--- game/target.py
+++ game/target.py
@@ -1,5 +1,23 @@
+import pygame
 from game_variables.GameVariables import GameVariables as gv
 
 
 class Target:
-    pass
+    def __init__(self, screen: pygame.Surface):
+        self.screen = screen
+        self.targets = Target
+        self.width = gv.SQUARE_SIZE
+        self.height = gv.SQUARE_SIZE
+        self.x_pos = gv.SCREEN_WIDTH // 2 - self.width // 2
+        self.y_pos = 30
+        self.direction = 1
+        self.health = 1
+
+    def get_rect(self):
+        return pygame.Rect(self.x_pos, self.y_pos, self.width, self.height)
+
+    def update_and_draw(self):
+        self.x_pos += 1.5 * self.direction
+        if self.x_pos <= 0 or self.x_pos >= gv.SCREEN_WIDTH - self.width:
+            self.direction *= -1
+        pygame.draw.rect(self.screen, "red", self.get_rect())
